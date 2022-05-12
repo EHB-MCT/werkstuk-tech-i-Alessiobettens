52a0da303bd8d38b901f495ec071d049d0aea28b
Alessiobettens <91527106+Alessiobettens@users.noreply.github.com>
2022-05-12 23:25:07 +0200

    Add files via upload

---
+++ Arduino code/sketch_may10b.ino
@@ -0,0 +1,18 @@
+
+int LED = A5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(LED, HIGH);
+delay(2000);
+digitalWrite(LED, LOW);
+delay(500);
+
+}
